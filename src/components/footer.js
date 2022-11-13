f5322334d96a7eb8c5497dc602c43a4c473a099d
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-11-13 21:53:05 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,23 @@
-import React from "react";
 
+// imports react
+import React from 'react';
+
+// class for the footer component// extends word used to export app.js
 export class Footer extends React.Component{
 
     render(){
+
+        // returning component
+         
         return(
             <div>
-                <h1>My Footer component</h1>
+              
+              <h1>This is the Footer Component!</h1>
+
             </div>
-        );
-    }
-}
+
+        );//return
+   
+    }// render
+
+}//class
